1442f8dbfb165cfbc48dd454fd82ad94aba1ddd2
kamalbennani <kamalbouchboy1@hotmail.fr>
2015-12-18 11:56:36 +0100

    init commit

---
+++ containers/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>This is cool</div>
+    );
+  }
+}
+
+export default App
